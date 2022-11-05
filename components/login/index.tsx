44afd2a0ef4fe58ffa58c86e5e028c59dd133239
d1av <davi4alves@gmail.com>
2022-11-05 17:06:50 -0300

    feat: new styles for login

--- components/login/index.tsx
+++ components/login/index.tsx
@@ -1,20 +1,44 @@
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
 import InputPublico from "../inputPublico";
-import { envelope } from "../../public/image";
+import { envelope, key, logo } from "../../public/image";
+import Button from "../button";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
   return (
     <section className={`paginaLogin paginaPublica`}>
-      <div className={`logoContainer`}></div>
+      <div className={`logoContainer`}>
+        <Image src={logo} alt="Instagram Logo" fill />
+      </div>
 
       <div className={`conteudoPaginaPublica`}>
         <form>
-          <InputPublico image={envelope}
-          tipo='email'
-          texto='Email'
-          aoAlterarValor={()=>console.log('digit email')}
-          
+          <InputPublico
+            image={envelope}
+            tipo="email"
+            texto="Email"
+            aoAlterarValor={(e: any) => setEmail(e.target.value)}
+            valor={email}
           />
+
+          <InputPublico
+            image={key}
+            tipo="password"
+            texto="Senha"
+            aoAlterarValor={(e: any) => setSenha(e.target.value)}
+            valor={senha}
+          />
+
+          <Button type="submit" text="Login" isDisabled={false} />
         </form>
+        <div className="rodapePaginaPublica">
+          <p>Não possui cadastro?</p>
+          <Link href="/cadastro">Faça seu cadastro agora</Link>
+        </div>
       </div>
     </section>
   );
