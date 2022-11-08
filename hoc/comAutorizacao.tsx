217568c3c8ef4184ddf088db1f642d0293b8bf40
d1av <davi4alves@gmail.com>
2022-11-08 20:08:52 -0300

    feat: add title and new favicon
    This commit adds the <Head> tag optimized
    for next/js and a new title for the pages
    called 'Devagram' and can be easily edited in the
    '/hoc/comAutorizacao.tsx', plus it does some SEO optimization
    using <meta> tags.

--- hoc/comAutorizacao.tsx
+++ hoc/comAutorizacao.tsx
@@ -4,11 +4,12 @@ import ApiUsuarioService from "../services/ApiUsuarioService";
 import { search } from "../public/image";
 import Footer from "../components/layout/Footer";
 import Loading from "../components/loading";
+import Head from "next/head";
 
 const usuarioService = new ApiUsuarioService();
 
-export default function comAutorizacao(Componente:any) {
-  return function Amigo (props:any){
+export default function comAutorizacao(Componente: any) {
+  return function Amigo(props: any) {
     const router = useRouter();
 
     if (typeof window !== "undefined") {
@@ -21,6 +22,18 @@ export default function comAutorizacao(Componente:any) {
 
       return (
         <>
+          <Head>
+            <title>Devagram</title>
+            <meta charSet="utf-8" />
+            <meta
+              name="viewport"
+              content="initial-scale=1.0, width=device-width"
+            />
+            <meta
+              name="description"
+              content="Blog inspirado onde é possível postar, editar e adicionar postagens com imagem."
+            ></meta>
+          </Head>
           <Header usuarioLogado={usuarioLogado} />
           <Loading />
           <Componente usuarioLogado={usuarioLogado} {...props} />
