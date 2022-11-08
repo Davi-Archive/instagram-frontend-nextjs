abe3ee0144dff95134675ab3407f918a11d68391
d1av <davi4alves@gmail.com>
2022-11-08 00:21:39 -0300

    feat: added classes

--- pages/publicacao/index.tsx
+++ pages/publicacao/index.tsx
@@ -1,33 +1,130 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { useState } from "react";
 import Button from "../../components/button";
 import HeaderComAcoes from "../../components/headerComAcoes";
 import UploadImagem from "../../components/uploadImage";
 import comAutorizacao from "../../hoc/comAutorizacao";
-import { createPost } from "../../public/image";
+import { createPost, avatar, leftArrow } from "../../public/image";
+import FeedService from "../../services/FeedService";
 
-const Publicacao = () => {
-  const [imagem, setImagem] = useState(true);
-  const [inputImagem, setInputImagem] = useState();
+const limiteDaDescricao = 255;
+const descricaoMinima = 3;
+const feedService = new FeedService();
+
+function Publicacao() {
+  const [imagem, setImagem] = useState();
+  const [descricao, setDescricao] = useState("");
+  const [inputImagem, setInputImagem] = useState<any | null>();
+  const [etapaAtual, setEtapaAtual] = useState(1);
+  const router = useRouter();
+
+  const estaNaEtapaUm = () => etapaAtual === 1;
+
+  const obterTextoEsquerdaCabecalho = () => {
+    if (estaNaEtapaUm() && imagem) {
+      return "Cancelar";
+    }
+
+    return "";
+  };
+
+  const obterTextoDireitaCabecalho = () => {
+    if (!imagem) {
+      return "";
+    }
+
+    if (estaNaEtapaUm()) {
+      return "Avançar";
+    }
+
+    return "Compartilhar";
+  };
+
+  const aoClicarAcaoEsquerdaCabecalho = () => {
+    if (estaNaEtapaUm()) {
+      inputImagem.value = null;
+      setImagem(null);
+      return;
+    }
+
+    setEtapaAtual(1);
+  };
+
+  const aoClicarAcaoDireitaCabecalho = () => {
+    if (estaNaEtapaUm()) {
+      setEtapaAtual(2);
+      return;
+    }
+
+    publicar();
+  };
+
+  const escreverDescricao = (e: any) => {
+    const valorAtual = e.target.value;
+    if (valorAtual.length >= limiteDaDescricao) {
+      return;
+    }
+
+    setDescricao(valorAtual);
+  };
+
+  const obterClassNameCabecalho = () => {
+    if (estaNaEtapaUm()) {
+      return "primeiraEtapa";
+    }
+
+    return "segundaEtapa";
+  };
+
+  const publicar = async () => {
+    try {
+      if (!validarFormulario()) {
+        alert(
+          "A descrição precisa de pelo menos 3 caracteres e a imagem precisa estar selecionada."
+        );
+        return;
+      }
+
+      const corpoPublicacao = new FormData();
+      corpoPublicacao.append("descricao", descricao);
+      corpoPublicacao.append("file", imagem.arquivo);
+
+      await feedService.fazerPublicacao(corpoPublicacao);
+      router.push("/");
+    } catch (error) {
+      alert("Erro ao salvar publicação!");
+    }
+  };
+
+  const validarFormulario = () => {
+    return descricao.length >= descricaoMinima && imagem?.arquivo;
+  };
+
   return (
-    <>
-      <div className="paginaPublicacao largura30pctDesktop">
-        <HeaderComAcoes
-          textoEsquerda="Cancelar"
-          elementoDireita="Avançar"
-          titulo="Nova Publicacao"
-        />
+    <div className="paginaPublicacao largura30pctDesktop">
+      <HeaderComAcoes
+        className={obterClassNameCabecalho()}
+        iconeEquerda={estaNaEtapaUm() ? null : leftArrow}
+        textoEsquerda={obterTextoEsquerdaCabecalho()}
+        aoClicarAcaoEsquerda={aoClicarAcaoEsquerdaCabecalho}
+        elementoDireita={obterTextoDireitaCabecalho()}
+        aoClicarElementoDireita={aoClicarAcaoDireitaCabecalho}
+        titulo="Nova publicação"
+      />
 
-        <hr className="linhaDivisoria" />
+      <hr className="linhaDivisoria" />
 
-        <div className="conteudoPaginaPublicacao">
+      <div className="conteudoPaginaPublicacao">
+        {estaNaEtapaUm() ? (
           <div className="primeiraEtapa">
             <UploadImagem
               setImagem={setImagem}
               aoSetarAReferencia={setInputImagem}
-              imagemPreviewClassName={!imagem ? "previewImagemPublicacao" : "previewImagemSelecionada"}
-              //@ts-ignore -- typescript has no access to dom
-              imagemPreview={imagem?.preview || createPost.src}
+              imagemPreviewClassName={
+                !imagem ? "previewImagemPublicacao" : "previewImagemSelecionada"
+              }
+              imagemPreview={imagem?.preview || avatar.src}
             />
 
             <span className="desktop textoDragAndDrop">
@@ -36,15 +133,31 @@ const Publicacao = () => {
 
             <Button
               type="button"
-              text="Selecionar Imagem"
-              //@ts-ignore -- use ref other component
-              onClick={() => inputImagem.click()}
+              text="Selecionar uma imagem"
+              onClick={() => inputImagem?.click()}
             />
           </div>
-        </div>
+        ) : (
+          <>
+            <div className="segundaEtapa">
+              <UploadImagem
+                setImagem={setImagem}
+                imagemPreview={imagem?.preview}
+              />
+
+              <textarea
+                rows={3}
+                value={descricao}
+                placeholder="Escreva uma legenda..."
+                onChange={escreverDescricao}
+              ></textarea>
+            </div>
+            <hr className="linhaDivisoria" />
+          </>
+        )}
       </div>
-    </>
+    </div>
   );
-};
+}
 
 export default comAutorizacao(Publicacao);
